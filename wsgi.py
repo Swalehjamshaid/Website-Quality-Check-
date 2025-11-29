0fac225975daee0ad22d81d471a0afcd7731047a
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-11-29 11:57:47 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,7 @@
 # wsgi.py
 
 from app import create_app
+import os
 
 # Call the factory function to create the application instance
 application = create_app(config_name=os.environ.get('FLASK_CONFIG', 'default'))
-
-# 'application' is now the callable Flask object that Vercel needs.
