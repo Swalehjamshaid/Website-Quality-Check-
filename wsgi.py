0d3dc0e9ee38257d719aaeb2b33a481e37dc3257
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-12-04 15:05:37 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,3 @@
 # wsgi.py
 from app import app
-
-# This makes it work on Vercel
 application = app
-
-# Force install WeasyPrint dependencies (important for future PDF)
-try:
-    import weasyprint
-    import cairo
-except ImportError:
-    pass
