55e006a286928ee1e00fc9f26ebbb975223c9062
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-12-07 02:33:30 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,2 @@
-# wsgi.py
-
-# Import the application factory function from your main application file
 from app import create_app
-
-# Create the application object instance using the factory function
 application = create_app()
