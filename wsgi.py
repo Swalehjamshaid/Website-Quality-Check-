9bc4a1bf9cc9f418a714eda0f1ee3ee537a5cd39
Swalehjamshaid <roy.jamshaid@gmail.com>
2025-12-07 12:26:31 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,8 @@
+# wsgi.py
 from app import create_app
+from app.tasks import celery
+
 application = create_app()
+
+# Important: Initialize Celery with the app context
+celery.conf.update(application.config)
